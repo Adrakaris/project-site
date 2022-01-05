7e1f7eaa38852631e2905f0940db1e4a94578265
Adrakaris <huyijun007@gmail.com>
2022-01-05 19:31:58 +0000

    Sort out a way of being able to program in typescript but still be able to run the website as usual
    and NOT have a 100MB+ node_modules file on my computer, or entirely reformatting the site

--- ts/scripts/darklight.ts
+++ ts/scripts/darklight.ts
@@ -1,6 +1,10 @@
-"use strict";
 // themes
-var dark = {
+
+// NOTE: light and dark have been switched around, because it is nicer to have dark theme by default
+
+type Theme = { [key: string]: string };
+
+var dark: Theme = {
     "--bg-col": "#ffffff",
     "--bg-active": "#f1f2e9",
     "--txt-col": "#000000",
@@ -12,8 +16,9 @@ var dark = {
     "--overlay": "#00000030",
     "--filter": "brightness(1) contrast(1)",
     "--img-bg": "#0000"
-};
-var light = {
+}
+
+var light: Theme = {  
     "--bg-col": "#2e2e2e",
     "--bg-active": "#333333",
     "--txt-col": "#eaeaea",
@@ -25,40 +30,51 @@ var light = {
     "--overlay": "#00000030",
     "--filter": "brightness(.8) contrast(1.2)",
     "--img-bg": "#fff"
-};
+}
+
+
 // code
-function setTheme(thema) {
+
+
+function setTheme(thema: Theme) {
     // iterate through every key value pair in the theme
     for (let key in thema) {
         // set the css variable to the value
         document.documentElement.style.setProperty(key, thema[key]);
     }
 }
-let style = document.documentElement.style;
+
+
+let style: CSSStyleDeclaration = document.documentElement.style
 // var cssRoot = styleSheet.querySelector(":root");  // document.querySelector("#theme-link").querySelector(":root");
-const toggle = document.getElementById("darkmode");
-const currentTheme = localStorage.getItem("theme");
-let indicator;
-if (currentTheme == "dark") { // seems redudnant but in case localStorage has none
-    indicator = "dark";
+const toggle: HTMLButtonElement = document.getElementById("darkmode") as HTMLButtonElement;
+const currentTheme: string|null = localStorage.getItem("theme");
+
+let indicator: string;
+
+if (currentTheme == "dark") {  // seems redudnant but in case localStorage has none
+    indicator = "dark"
     setTheme(dark);
-}
-else {
-    indicator = "light";
+} else {
+    indicator = "light"
     setTheme(light);
 }
-toggle.addEventListener("click", function () {
+
+
+
+toggle.addEventListener("click", function() {
     // we need to check if it's dark or light
     if (indicator == "dark") {
         indicator = "light";
         toggle.innerText = "Light Mode";
         localStorage.setItem("theme", "light");
         setTheme(light);
-    }
-    else {
+    } else {
         indicator = "dark";
         toggle.innerText = "Dark Mode";
         localStorage.setItem("theme", "dark");
         setTheme(dark);
     }
 });
+
+
--- ts/scripts/readprojects.js
+++ ts/scripts/readprojects.js
@@ -1,14 +1,16 @@
-"use strict";
-var MW = 300;
-var MH = 200;
+var MW = 300
+var MH = 200
+
+
 function scaleWidthHeight(imgElem) {
     let natWid = imgElem.naturalWidth;
     let natHei = imgElem.naturalHeight;
     // console.log(natWid, natHei);
     let resizeFactorWidth = MW / natWid;
+    
     // resize by width factor, if the image is wider than MH then resize by height as well
-    let wid = natWid * resizeFactorWidth;
-    let hei = natHei * resizeFactorWidth;
+    let wid = natWid * resizeFactorWidth
+    let hei = natHei * resizeFactorWidth
     if (hei > MH) {
         let resizeFactorHeight = MH / hei;
         wid *= resizeFactorHeight;
@@ -18,12 +20,16 @@ function scaleWidthHeight(imgElem) {
     imgElem.height = hei;
     // if the height is less than MH, add a margin to top and bottom
     if (hei < MH) {
-        let finalMargin = (MH - hei) / 2;
+        let finalMargin = (MH - hei)/2;
         imgElem.style.marginTop = `${finalMargin}px`;
         imgElem.style.marginBottom = `${finalMargin}px`;
     }
 }
+
+
 function addProjects(projects) {
+    
+
     let addList = document.getElementById("genprojects");
     projects.reverse().forEach(project => {
         // creation
@@ -31,9 +37,11 @@ function addProjects(projects) {
         pdiv.classList.add("buttonwrapper");
         pdiv.classList.add("white");
         // pdiv.style.width = "30%";
+
         let pa = document.createElement("a");
         pa.setAttribute("href", project["link"]);
         pdiv.appendChild(pa);
+        
         let pimg = document.createElement("img");
         pimg.src = project["image"];
         pimg.style.maxWidth = `${MW}px`;
@@ -41,48 +49,60 @@ function addProjects(projects) {
         pimg.style.imageRendering = "pixelated";
         pimg.style.resizeMode = "contain";
         // scaleWidthHeight(pimg);
-        pimg.onload = function () {
+        pimg.onload = function() {
             scaleWidthHeight(this);
         };
         pa.appendChild(pimg);
+        
+
         let ptitle = document.createElement("h3");
         ptitle.innerText = project["title"];
         ptitle.style.textAlign = "left";
         pa.appendChild(ptitle);
+
         let pLanguage = document.createElement("p");
         pLanguage.classList.add("subheading");
         pLanguage.innerText = project["language"];
         pLanguage.style.fontStyle = "italic";
         pLanguage.style.textAlign = "left";
         pa.appendChild(pLanguage);
+
         let pDescription = document.createElement("p");
         pDescription.innerText = project["description"];
         pDescription.style.textAlign = "left";
         pa.appendChild(pDescription);
+
         // insertion
         addList.appendChild(pdiv);
     });
+
     // fade in
     let proj = document.querySelectorAll("#genprojects > div");
-    let i = 1;
+    let i = 1
     proj.forEach(projn => {
         setTimeout(() => {
             projn.classList.add("visible");
         }, 350 * i);
-        i++;
-    });
+        
+        i++
+    })
+    
 }
+
+
 // oh to get a simple JSON file
 let jsonObj;
 let httpRequest = new XMLHttpRequest();
 httpRequest.open("GET", "./scripts/projects.json", true);
 httpRequest.send();
-httpRequest.addEventListener("readystatechange", function () {
+httpRequest.addEventListener("readystatechange", function() {
     if (this.readyState === this.DONE) {
         jsonObj = JSON.parse(this.response);
         // console.log(jsonObj);
         addProjects(jsonObj["projects"]);
     }
-});
+})
+
 // projects.forEach(element => {
+    
 // });
